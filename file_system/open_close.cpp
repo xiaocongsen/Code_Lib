8d99726fa356145184fb414d285284c1c781d549
xiaocs <759434609@qq.com>
2016-06-29 08:21:37 +0800

    20160629

--- file_system/open_close.cpp
+++ file_system/open_close.cpp
@@ -1,17 +1,19 @@
 #include<stdio.h>
 #include<sys/types.h>
 #include<sys/stat.h>
+#include<unistd.h>
 #include<fcntl.h>
 int main()
 {
-	const char *pathname = "./open";
-	int flags = O_RDONLY;
-	int fg = open(pathname, flags);
+	const char *pathname = "./open"; //路径
+	int flags = O_RDONLY;		//只读方式打开；
+	int fg = open(pathname, flags);//打开文件
 	if(fg < 0)
 	{
 		perror("open error");
 		return -1;
 	}
 	printf("open success");
+	close(fg);//关闭文件
 	return 0;
 }
