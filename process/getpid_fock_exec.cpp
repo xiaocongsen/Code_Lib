ce1055d30428e1c0fec7c437c68681ba3fd90895
xiaocs <759434609@qq.com>
2016-06-30 08:19:16 +0800

    20160630

--- process/getpid_fock_exec.cpp
+++ process/getpid_fock_exec.cpp
@@ -3,22 +3,29 @@
 #include<unistd.h>
 int main()
 {
-	gid_t gid = getgid();//实际用户主
-	gid = getegid();
+	gid_t gid1 = getgid();//实际用户组id
+	printf("主程序的实际gid = %d\n",gid1);
+	gid_t gid = getegid();  //有效用户组id
+	printf("主程序的有效gid = %d\n",gid);
 	pid_t pid = getppid();
-	printf("主程序的父的pid = %d\n",gid);
+	printf("主程序的父的pid = %d\n",pid);
 	pid = getpid();
-	printf("主程序的pid = %d\n",gid);
-	gid = fork();
-	printf("fork 返回值的pid = %d\n",gid);
+	printf("主程序的pid = %d\n",pid);
+	pid = fork();
+	printf("fork 返回值的pid = %d\n",pid);
 	if(pid == 0)
 	{
 		pid = getpid();
-		printf("pid == 0分支的pid = %d\n",gid);
+		printf("的pid == %d\n",pid);
+		gid_t gid = getgid();//实际用户组id
+		printf("pid == 0分支的实际gid = %d\n",gid);
+		gid = getegid();  //有效用户组id
+		printf("pid == 0分支的有效gid = %d\n",gid);
 	}
 	else
 	{
+		sleep(1);
 		pid = getpid();
-		printf("pid != 0分支的pid = %d\n",gid);
+		printf("pid != 0分支的pid = %d\n",pid);
 	}
 }
